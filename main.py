b91d85ff36af857175eaa594d3085a3f1ea40a32
glebmish <bboygleb@gmail.com>
2016-05-01 13:56:49 +0300

    minor changes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import Splitter
 
-with open('./Examples/Bayes_Classifier.py', 'r') as file:
+with open('./Examples_train/Bayes_Classifier.py', 'r') as file:
     string = file.read()
     words = Splitter.detect_const(Splitter.imp_split(Splitter.delimiters, string, Splitter.detached))
     print words
