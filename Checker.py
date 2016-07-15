5c76f2f7452fdb761369c3b886306a29cc6e35a9
glebmish <bboygleb@gmail.com>
2016-07-15 18:47:01 +0300

    Implemented multidict: dict with uncertain dimension where all operations performed with recursion. Refactored __init__ and train, removed hardcoded window size = 3, now it can be specified in __init__ and it will be working accordingly

--- Checker.py
+++ Checker.py
@@ -1,83 +1,136 @@
-from collections import defaultdict
+from collections import defaultdict, deque, namedtuple
 import Splitter
 
-class Checker():
 
-    def __init__(self):
+def inc(x):
+    return x + 1
+
+def dec(x):
+    return x - 1
+
+def id(x):
+    return x
+
+def multidict_operation(mdict, indexes, operation):
+    if len(indexes) == 1:
+        mdict[indexes[0]] = operation(mdict[indexes[0]])
+        return mdict[indexes[0]]
+    return multidict_operation(mdict[indexes[0]], indexes[1:], operation)
+
+
+def multidict_cycle(mdict, dimensions, operation):
+    if dimensions == 1:
+        for key in mdict:
+            mdict[key] = operation(mdict[key])
+        return
+    for key in mdict:
+        multidict_cycle(mdict[key], dimensions - 1, operation)
+
+
+def print_cell(x, indexes):
+    print('counters' + repr(indexes) + ': ' + repr(x))
+    return
+
+
+def multidict_cycle_indexes(mdict, dimensions, operation, indexes=[]):
+    if dimensions == 1:
+        for key in mdict:
+            mdict[key] = operation(mdict[key], indexes + [key])
+        return
+    for key in mdict:
+        multidict_cycle_indexes(mdict[key], dimensions - 1, operation, indexes + [key])
+
+
+def multidict(dimensions):
+    if dimensions == 1:
+        return defaultdict(lambda : 0.0)
+    return defaultdict(lambda : multidict(dimensions - 1))
+
+class Checker:
+
+    def __init__(self, window_left=1, window_right=1):
         self.unknown = 'unknown word'
 
-        self.dictionary = set('\n')
-        self.counters = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: 0)))
-
-        self.window_left = 1
-        self.window_right = 1
+        self.window_left = window_left
+        self.window_right = window_right
+        self.window_len = self.window_left + self.window_right + 1
 
         self.dict_delay = self.window_left
+
+        self.bound_probability = 0.01
+
+        self.dictionary = set('\n')
+        self.counters = multidict(self.window_left + 1 + self.window_right)
 
     def train(self, filenames):
         for each in filenames:
             file = open(each, 'r');
             string = file.read()
 
-            words = Splitter.detect_const(Splitter.imp_split(
-                Splitter.delimiters, string, Splitter.detached
-            ))
+            words = Splitter.imp_split(Splitter.detect_const(string))
 
-            window = ['\n', '\n', words[0]]
+            # initialize window
+            window = ['\n'] * (self.window_left + 1)
+            for word in words[:self.window_right]:
+                window.append(word)
+            cur_word_index = self.window_left
 
-            dict_delayed = []
+            # initialize queue for delayed words and declare class for this words
+            dict_delayed = deque()
+            class Delayed:
+                def __init__(self, word, delay):
+                    self.word = word
+                    self.delay = delay
 
-            for new_word in words[1:]:
-                window[0] = window[1]
-                window[1] = window[2]
-                window[2] = new_word
+            # process training for each word
+            for new_word in words[self.window_right:]:
+                # shift window
+                for i in range(0, self.window_len - 1):
+                    window[i] = window[i + 1]
+                window[self.window_len - 1] = new_word
 
-                print 'Window:  [{}, {}, {}]'.format(repr(window[0]), repr(window[1]), repr(window[2]))
+                # debug print
+                print('Window:  ' + repr(window))
 
-                if window[1] in self.dictionary:
-                    index_cur = window[1]
-                else:
-                    index_cur = self.unknown
+                # make indexes (current is first)
+                indexes = [window[cur_word_index] if window[cur_word_index] in self.dictionary else self.unknown]
+                for word in window:
+                    if word is not window[cur_word_index]:
+                        indexes.append(word if word in self.dictionary else self.unknown)
 
-                if window[0] in self.dictionary:
-                    index_prev = window[0]
-                else:
-                    index_prev = self.unknown
+                # debug print
+                print('Indexes: ' + repr(indexes))
 
-                if window[2] in self.dictionary:
-                    index_next = window[2]
-                else:
-                    index_next = self.unknown
+                # increment to cell pointed by list of indexes
+                multidict_operation(self.counters, indexes, inc)
 
-                print 'Indexes: [{}] [{}] [{}]'.format(repr(index_cur), repr(index_prev), repr(index_next))
+                # debug print
+                print('Counter: ' + repr(multidict_operation(self.counters, indexes, id)))
 
-                self.counters[index_cur][index_prev][index_next] += 1
+                # adding current word to delayed dict if necessary
+                if window[cur_word_index] not in self.dictionary:
+                    cur_word_delay = self.dict_delay - sum([delayed.delay for delayed in dict_delayed])
+                    dict_delayed.append(Delayed(window[cur_word_index], cur_word_delay))
 
-                print 'Counter: {}'.format(repr(self.counters[index_cur][index_prev][index_next]))
+                # move first object to dictionary if delay is expired
+                if len(dict_delayed) and dict_delayed[0].delay == 0:
+                    self.dictionary.add(dict_delayed[0].word)
+                    dict_delayed.popleft()
+                if len(dict_delayed):
+                    dict_delayed[0].delay -= 1
 
-                for i in range(len(dict_delayed)):
-                    dict_delayed[i] = (dict_delayed[i][0], dict_delayed[i][1] - 1)
+                # debug print
+                print('Dict:    ' + repr(self.dictionary))
+                print('Delayed: ' + repr([delayed.word for delayed in dict_delayed]))
+                print('\n')
 
-                    if dict_delayed[i][1] == 0:
-                        self.dictionary.add(dict_delayed[i][0])
-                        dict_delayed.pop(i)
+        # decrement each cell in counters[self.unknown]
+        multidict_cycle(self.counters[self.unknown], self.window_len - 1, dec)
 
-                dict_delayed.append((window[1], 1))
+        # debug print
+        multidict_cycle_indexes(self.counters, self.window_len, print_cell)
 
-                print 'Dict:    {}'.format(self.dictionary)
-                print 'Delayed: {}'.format(map(lambda x: x[0], dict_delayed))
-                print '\n\n'
-
-        for index_prev in self.counters[self.unknown]:
-            for index_next in self.counters[self.unknown][index_prev]:
-                self.counters[self.unknown][index_prev][index_next] -= 1
-
-        for index_cur in self.counters:
-            for index_prev in self.counters[index_cur]:
-                for index_next in self.counters[index_cur][index_prev]:
-                    print 'counters[{}][{}][{}]: {}'.format(repr(index_cur), repr(index_prev), repr(index_next),
-                                                            self.counters[index_cur][index_prev][index_next])
-
+#TODO: refactor check
     def check(self, filenames):
         print '\n\n'
 
@@ -131,4 +184,4 @@ class Checker():
 
 cc = Checker()
 cc.train(['Examples_train/easy_ex.py'])
-cc.check(['Examples_check/easy_ch.py'])
+#cc.check(['Examples_check/easy_ch.py'])
